d0ad821fef4a327f0e92e69aef23db22d1172d59
summersonnn <kyazogluu@gmail.com>
2019-12-19 22:28:21 +0300

    Target Q Network implemented

--- main.py
+++ main.py
@@ -7,34 +7,31 @@ import torch.optim as optim
 import torch.nn.functional as F
 import dqn
 import minichess
-import qvalues
+import unrelatedmethods
 import fileoperations
 
 
 PATH_TO_DIRECTORY = "pretrained_model/"
-batch_size = 64 #make this smth like 256 when ready
+batch_size = 4 #make this smth like 256 when ready
 gamma = 1 #set this to 0.999 or near if you want stochasticity. 1 assumes same action always result in same rewards -> future rewards are NOT discounted
 eps_start = 1	#maximum (start) exploration rate
 eps_end = 0.01	#minimum exploration rate
 eps_decay = 0.001 #higher decay means exploration is LESS probable, exploitation is MORE LIKELY
-#target_update = 20	#use this if you're using two networks, one named target network
+target_update = 20	#how often does target network get updated? (in terms of episode number) This will also be used in creating model files
 memory_size = 100000 #memory size to hold each state,action,next_state, reward, terminal tuple
 lr = 0.001 #how much to change the model in response to the estimated error each time the model weights are updated
-num_episodes = 51
+num_episodes = 501
 max_steps_per_episode = 301
 
-def train():
-	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def train(policy_net, target_net):
 	em = minichess.MiniChess(device)	#setting up the environment
 	strategy = dqn.EpsilonGreedyStrategy(eps_start, eps_end, eps_decay) 
 	agent = dqn.Agent(strategy, device)
 	memory = dqn.ReplayMemory(memory_size)
 
-	#Initialize net, optimizer and load them if exist.
-	policy_net = dqn.DQN().to(device)
 	optimizer = optim.Adam(params=policy_net.parameters(), lr=lr)
-	past_episodes = 0
-	last_trained_model = fileoperations.find_last_edited_file(PATH_TO_DIRECTORY)
+	past_episodes = 0	#how many episode is played before? this variable may be changed in the upcoming if block.
+	last_trained_model = fileoperations.find_last_edited_file(PATH_TO_DIRECTORY)	#Returns the last_trained model from multiple models.
 
 	if last_trained_model is not None:
 		print("***Last trained model: " + last_trained_model)
@@ -43,7 +40,9 @@ def train():
 		optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 		past_episodes = checkpoint['episode']
 		loss = checkpoint['loss']
-		policy_net.train()
+		
+	#Weights and biases in the target net is same as in policy net
+	target_net.load_state_dict(policy_net.state_dict())
 
 	steps_per_episode =[]	#counts how many steps played in each episode
 	losses = []
@@ -66,13 +65,16 @@ def train():
 					print("----------BATCHING-------")
 					flag = False
 				experiences = memory.sample(batch_size)	#sample experiences from memory
-				states, actions, rewards, next_states = qvalues.extract_tensors(experiences)	#extract them
+				states, actions, rewards, next_states = unrelatedmethods.extract_tensors(experiences)	#extract them
 
-				current_q_values = qvalues.QValues.get_current(policy_net, states, actions)	#get the current q values to calculate loss afterwards
-				# get output for the next state
-				next_states = policy_net(states)
+				#get the current q values to calculate loss afterwards
+				current_q_values = policy_net(states).gather(dim=-1, index=actions.unsqueeze(-1))
+				# get output(q-values) for the next state. WARNING! Some terminal states may have been passed to target_net. But final states
+				# don't have any q-values since there can be no action to take from terminal states. In the upcoming lines, we spot terminal states
+				# and don't take their garbage q-values. Instead, we take only immediate rewards. Is this the best approach?
+				next_q_values = target_net(next_states)
 
-				# set y_j to r_j for terminal state, otherwise to r_j + gamma*max(Q)
+				# set y_j to r_j for terminal state, otherwise to r_j + gamma*max(Q). Garbage q-values for terminal states are not used.
 				target_q_values = torch.cat(tuple(rewards[i].unsqueeze(0) if experiences[i][4]
 								else rewards[i].unsqueeze(0) + gamma * torch.max(next_states[i]).unsqueeze(0)
 								for i in range(len(experiences))))
@@ -96,14 +98,17 @@ def train():
 				#print(state)
 				break
 
-		if( episode % 10 == 0):
-			print("Episode:" + str(episode) + " -Weights are updated!")
+		#Update target network with weights and biases in the policy network
+		#Also create new model files
+		if( episode % target_update == 0):
+			target_net.load_state_dict(policy_net.state_dict())
 			torch.save({ 'episode': episode,
 	            		'model_state_dict': policy_net.state_dict(),
 	            		'optimizer_state_dict': optimizer.state_dict(),
 	            		'loss': loss, }, PATH_TO_DIRECTORY + "MiniChess-trained-model" + str(episode) + ".tar"
 						)
-		print("Last state: " + str(em.get_unnormalized_state()))
+			print("Episode:" + str(episode) + " -Weights are updated!")
+
 		print("Steps per episode: " + str(steps_per_episode)+ '\n')
 	plt.plot(losses)
 	plt.ylabel('Losses')
@@ -111,7 +116,14 @@ def train():
 	return None
 
 if __name__ == '__main__':
-	train()
+
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	#Initialize policy and target net. Target net will work in eval mode and will not update the weights (on its own)
+	policy_net = dqn.DQN().to(device)
+	target_net = dqn.DQN().to(device)
+	target_net.eval()
+
+	train(policy_net, target_net)
 	#sys.exit() or raise SystemExit doesn't work for some reason. That's the only way I could end the process.
 	os.kill(os.getpid(), signal.SIGTERM)
 
---
+++ unrelatedmethods.py
@@ -0,0 +1,28 @@
+import torch
+import dqn
+
+def extract_tensors(experiences):
+	# Convert batch of Experiences to Experience of batches
+	batch = dqn.Experience(*zip(*experiences))
+	state_batch = []
+	nextState_batch = []
+
+	for d in experiences:
+		#for state batch
+		a = d[0].cpu().detach().numpy()
+		a = [a]
+		state_batch += a
+		#for nextState batch
+		b = d[2].cpu().detach().numpy()
+		b = [b]
+		nextState_batch += b
+
+	state_batch = torch.tensor(state_batch, dtype=torch.float32)
+	action_batch = torch.cat(tuple(d[1] for d in experiences))
+	nextState_batch = torch.tensor(nextState_batch, dtype=torch.float32)
+	reward_batch = torch.cat(tuple(d[3] for d in experiences))
+	
+	return (state_batch,action_batch,reward_batch,nextState_batch)
+
+
+
